303f8474dadad3dc3bfa82df9a183a8c58f768bb
trichoplax <xalpohcirt@gmail.com>
2025-12-02 00:31:21 +0000

    Include words ending in 'sity' rather than just 'city'

--- city_namer.py
+++ city_namer.py
@@ -1,29 +1,31 @@
 from random import sample
 
 WORD_FILE_NAME = "/usr/share/dict/words"
-    
-    
+
+
 def city_names(number=None):
-    with open(WORD_FILE_NAME, 'r') as word_file:
+    with open(WORD_FILE_NAME, "r") as word_file:
         words = word_file.read().splitlines()
-    
-    cities = [word[:-4] for word in words if (
-        len(word) > 4
-        and word.endswith('city')
-        and word.isalpha()
-        and word.islower()
-    )]
-    
-    city_count = max(0, min(
-        1 if number is None else int(number),
-        len(cities)
-    ))
-        
+
+    cities = [
+        word[:-4]
+        for word in words
+        if (
+            len(word) > 4
+            and (word.endswith("city") or word.endswith("sity"))
+            and word.isalpha()
+            and word.islower()
+        )
+    ]
+
+    city_count = max(0, min(1 if number is None else int(number), len(cities)))
+
     return sorted(sample(cities, city_count))
 
 
-if __name__ == '__main__':
-    import sys   
+if __name__ == "__main__":
+    import sys
+
     arguments = sys.argv[1:]
     if len(arguments) > 0:
         argument = arguments[0]
